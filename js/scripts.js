86f1e98e2fe449743df010e374105cd4bc0b321e
Filip Nagalewski <nagalewski@gmail.com>
2017-03-25 21:42:52 +0100

    it finally works

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 function getTriangleArea(a, h) {
-  var a,
-    h,
-    triangleArea;
+  var triangleArea = a*h/2;
 
-    if (a > 0 && h > 0) {
-      triangleArea = a*h/2;
-      console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea),
-      alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-    } else if (a <= 0 || h <= 0) {
-      console.log('Nieprawidłowe dane'),
-      alert('Nieprawidłowe dane');
-    }
-    return triangleArea;
+  if (a > 0 && h > 0) {
+    triangleArea = a*h/2;
+    console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea),
+    alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+  } else if (a <= 0 || h <= 0) {
+    console.log('Nieprawidłowe dane'),
+    alert('Nieprawidłowe dane');
+  }
+  return triangleArea;
 }
 
-console.log(getTriangleArea(10, 6));
+getTriangleArea(10,6);
 
 var triangle1Area = getTriangleArea(8, 15);
 var triangle2Area = getTriangleArea(4, 9);
